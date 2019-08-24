34d2d18d5d9ab2d2864d054650c021a78450ee87
Anthony Panettiere <azezpz1@gmail.com>
2019-08-24 15:14:13 -0500

    Created CSVRow Constructor

--- CSVRow.cpp
+++ CSVRow.cpp
@@ -1,1 +1,6 @@
 #include "CSVRow.h"
+
+CSVRow::CSVRow(std::vector<std::string> ColumnNames)
+{
+	int row_length = ColumnNames.size();
+}
--- CSVRow.h
+++ CSVRow.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <vector>
+#include <string>
+
 class CSVRow
 {
+public:
+	CSVRow(std::vector<std::string> ColumnNames);
 };
 
