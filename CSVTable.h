eefdedb891af0eabe7ca5fc00ff0672e4bab48b6
Anthony Panettiere <azezpz1@gmail.com>
2019-08-24 18:07:02 -0500

    Adding basic public interface with CSVTable

--- CSVTable.h
+++ CSVTable.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include <fstream>
+#include <string>
+#include "CSVRow.h"
+#include "CSVColumn.h"
+
 class CSVTable
 {
+public:
+	CSVTable(std::ifstream CSVFileStream);
+	std::string GetElement(int column_id, int row_id );
+	CSVRow GetRow(int row_id);
+	CSVColumn GetColumn(int column_id);
+
 };
 
