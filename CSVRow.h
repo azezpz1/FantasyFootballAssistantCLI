8917f1a468cdebc5224a1e6639f38f185fa96181
Anthony Panettiere <azezpz1@gmail.com>
2019-08-26 05:59:02 -0500

    Adding elements field Adidng string to row constructor

--- CSVRow.h
+++ CSVRow.h
@@ -5,6 +5,9 @@
 class CSVRow
 {
 public:
-	CSVRow(std::vector<std::string> ColumnNames);
+	CSVRow(std::vector<std::string> ColumnNames, std::string line);
+	
+private:
+	std::vector<std::string> elements;
 };
 
