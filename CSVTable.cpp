886acefb948fb721f27c247b940facbb6645129f
Anthony Panettiere <azezpz1@gmail.com>
2019-08-27 06:29:55 -0500

    Creating vector of CSVRows which makes up the table

--- CSVTable.cpp
+++ CSVTable.cpp
@@ -1,5 +1,24 @@
 #include <sstream>
 #include "CSVTable.h"
+#include "CSVRow.h"
+
+CSVTable::CSVTable(std::ifstream CSVFileStream)
+{
+	std::string name_line;
+	std::getline(CSVFileStream, name_line);
+
+	std::vector<std::string> ColumnNames = ConvertColumnNamesStringToVector(name_line);
+	
+	std::vector<CSVRow> rows;
+
+	std::string line;
+	while (std::getline(CSVFileStream, line))
+	{
+		CSVRow row = CSVRow::CSVRow(ColumnNames, line);
+		rows.push_back(row);
+	}
+}
+
 std::vector<std::string> CSVTable::ConvertColumnNamesStringToVector(std::string column_name_line)
 {
 	std::stringstream stream(column_name_line);
